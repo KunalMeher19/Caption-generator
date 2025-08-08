5ad83ce691f8f26d86ca2d463ff00292f95d48d6
Ardhendu Abhishek Meher <meherardhenduabhishek@gmail.com>
2025-08-09 01:17:40 +0530

    Makes some changes bu creating loginController

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require('../models/user.models')
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 async function registerController(req, res) {
     const { username, password } = req.body;
@@ -7,19 +8,19 @@ async function registerController(req, res) {
     const isUser = await userModel.findOne({
         username
     });
-    if(isUser){
+    if (isUser) {
         return res.status(400).json({
             message: "User already exists"
         })
     }
     const user = await userModel.create({
         username,
-        password
+        password: await bcrypt.hash(password, 10)
     });
     const token = jwt.sign({
-        id:user._id
-    },process.env.JWT_SECRET)
-    res.cookie("token",token)
+        id: user._id
+    }, process.env.JWT_SECRET)
+    res.cookie("token", token)
 
     return res.status(201).json({
         message: "user created successfully",
@@ -28,7 +29,40 @@ async function registerController(req, res) {
 }
 
 async function loginController(req, res) {
+    const { username, password } = req.body;
 
+    const user = await userModel.findOne({
+        username
+    })
+    if(!user){
+        return res.status(400).json({
+            message: "user not found"
+        })
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if(!validPassword){
+        return res.status(400).json({
+            message: "Inalid password"
+        })
+    }
+
+    const token = jwt.sign({
+        id:user._id,
+    },process.env.JWT_SECRET);
+    res.cookie("token",token);
+
+    return res.status(200).json({
+        message: "Logged in successfully",
+        user:{
+            username: user.username,
+            id: user._id
+        }
+    })
+}
+
+async function logoutController(req,res){
+    
 }
 
 module.exports = {
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const route = express.Router();
 const userModel = require('../models/user.models')
 const jwt = require('jsonwebtoken')
-const { registerController } = require('../controllers/auth.controller')
+const { registerController, loginController } = require('../controllers/auth.controller')
 
 // POST /register
 // POST /login
 // GET /user [Protected]
 
-route.post('/register',registerController)
+route.post('/register', registerController)
+route.post('/login', loginController)
 
 module.exports = route
