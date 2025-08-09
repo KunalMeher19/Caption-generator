c8c54a12439b13b32e1d71a49cc4070f00691233
Ardhendu Abhishek Meher <meherardhenduabhishek@gmail.com>
2025-08-10 01:29:06 +0530

    Remved api

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -2,7 +2,6 @@ const { GoogleGenAI } = require("@google/genai");
 
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
 const ai = new GoogleGenAI({
-    apiKey: "AIzaSyCH-E-y0k03K-fD3nk8bkdqkgZx0D9R3GA"
 });
 
 async function generateCaption(base64ImageFile) {
